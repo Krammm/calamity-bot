037151d314d3ee50d9672ef80dee2b555c1d5661
NinjaSbrillo <64222250+NinjaSbrillo@users.noreply.github.com>
2020-04-29 15:32:48 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,10 +74,10 @@ client.on('message', message => {
         message.channel.send('Il messaggio ha ' + lunghezzamess + ' caratteri.');
         break;
       case 'ban':
-        if(admin.includes(message.author.id))
+        if(admin.includes(message.author.id)){
             const user = message.mentions.users.first();
             message.guild.ban(user);
-        else message.channel.send("Mi dispiace testa di cazzo non lo puoi fare");
+        }else message.channel.send("Mi dispiace testa di cazzo non lo puoi fare");
         break;
       case 'addrole':
             try { 
