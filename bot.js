b2541aa79e48db477f51ec421c725e052c12c13a
NinjaSbrillo <verioserio@gmail.com>
2020-05-04 23:00:46 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 // boh
 const client = new Discord.Client();
 const prefix = '!';
-const admin = [509779445557493821, 544579907133636619, 296353825550237697,211131331155984385];
+const admin = [509779445557493821, 544579907133636619, 296353825550237697, 211131331155984385];
 var insulti = ['Quel frocione di lovy', ' :poop:', 'Ecco adesso mi devo fare la doccia...', 'Perchè parli della merda?'];
 var command_channel = 704623913614704731;
 
@@ -14,149 +14,155 @@ client.on('ready', () => {
 
 client.on('message', message => {
 
-  if(message.author.id === client.user.id) {return;}
+    if (message.author.id === client.user.id) { return; }
 
-  if (message.isMemberMentioned(client.user)) {
+    if (message.isMemberMentioned(client.user)) {
 
-    message.reply('cazzo vuoi?');
+        message.reply('cazzo vuoi?');
 
-  }
-
-  command = message.content.toLowerCase()
-
-  if (command.startsWith(prefix)) { // Se il messaggio è un comando
-      //if(!command.channel.name.includes("💻comandi💻")){
-      //  message.reply("testa di cazzo non ti permettere!");
-      //}
-
-    var line = command;
-    line = line.substring(1);
-    var args = message.content.trim().split(/ +/g);
-    command = args[0];
-    command = command.substring(1);
-    var user = message.mentions.members.first(); 
-    let role = message.guild.roles.find(r => r.name === args[2]);
-
-    switch(command) {
-      case 'aiuto':
-        message.channel.send('I comandi disponibili sono: aiuto, ping, dado, timer, lunghezza.');
-        break;
-      case 'ping':
-        apiPing = Math.round(message.client.ping);
-        responseTime = Math.round(Date.now() - message.createdTimestamp);
-        message.channel.send(`**Ping:** \`${responseTime}ms\``);
-        break;
-      case 'dado':
-        var x = Math.random();
-        x = x * 6 + 1;
-        x = parseInt(x);
-        message.channel.send('É uscito ' + x);
-        break;
-      case 'timer':
-        var time = parseInt(args[1]);
-        line = line.replace('timer ', '');
-        line = line.replace(args[1] + ' ', '');
-        var reason = line;
-        if (line == '') { 
-
-          message.channel.send('Ti manderò un messaggio fra ' + time + ' secondi.');        
-          setTimeout(function() {
-          message.reply("il timer è finito.");
-          }, time * 1000);
-
-        } else {
-          message.channel.send('Ti manderò un messaggio fra ' + time + ' secondi, per il seguente motivo: "' + reason + '".');
-          setTimeout(function() {
-          message.reply('il timer è finito. Motivo: "' + reason + '".');
-          }, time * 1000);
-        }  
-        break;
-      case 'lunghezza':
-        line = line.replace('lunghezza ', '');
-        var lunghezzamess = line.length;
-        message.channel.send('Il messaggio ha ' + lunghezzamess + ' caratteri.');
-        break;
-      case 'ban':
-        if(admin.includes(message.author.id)){
-            const user = message.mentions.users.first();
-            message.guild.ban(user);
-        }else message.channel.send("Mi dispiace testa di cazzo non lo puoi fare");
-        break;
-      case 'addrole':
-            try { 
-                user.addRole(role);
-                message.channel.send('Ho aggiunto ' + user + " al ruolo " + role + ".");
-                } catch(error) {
-                    console.log(error);
-                    message.channel.send("Sono troppo insicura per farlo...");
-                    message.channel.send("ah, è ho anche riscontrato un errore");
-                    //message.channel.send("@Krammm , @NinjaSbrillo non sapete neanche programmarmi")
-                }
-             break;
-      case 'removerole':
-            try { 
-                user.removeRole(role);
-                message.channel.send('Ho tolto ' + user + " dal ruolo " + role + ".");
-                } catch(error) {
-                    console.log(error);
-                    message.channel.send("Sono troppo insicura per farlo...");
-                    message.channel.send("ah, è ho anche riscontrato un errore");
-                    //message.channel.send("@Krammm , @NinjaSbrillo non sapete neanche programmarmi")
-                }
-            break;
-        case 'clear':
-            try {
-                async function clear() {
-                 message.delete();
-                 const fetched = await message.channel.fetchMessages({limit: 99});
-                 message.channel.bulkDelete(fetched);
-                }
-            clear();
-            } catch(error) {
-                console.log(error);
-                message.channel.send("Errore nell'eliminazione dei messaggi.");
-            }
-            break;
-      default:
-        message.channel.send("Command: " + command + " Line: " + line);
-        break;
     }
-  }
+
+    command = message.content.toLowerCase()
+
+    if (command.startsWith(prefix)) { // Se il messaggio è un comando
+        //if(!command.channel.name.includes("💻comandi💻")){
+        //  message.reply("testa di cazzo non ti permettere!");
+        //}
+
+        var line = command;
+        line = line.substring(1);
+        var args = message.content.trim().split(/ +/g);
+        command = args[0];
+        command = command.substring(1);
+        var user = message.mentions.members.first();
+        let role = message.guild.roles.find(r => r.name === args[2]);
+
+        switch (command) {
+            case 'aiuto':
+                message.channel.send('I comandi disponibili sono: aiuto, ping, dado, timer, lunghezza.');
+                break;
+            case 'ping':
+                apiPing = Math.round(message.client.ping);
+                responseTime = Math.round(Date.now() - message.createdTimestamp);
+                message.channel.send(`**Ping:** \`${responseTime}ms\``);
+                break;
+            case 'dado':
+                var x = Math.random();
+                x = x * 6 + 1;
+                x = parseInt(x);
+                message.channel.send('É uscito ' + x);
+                break;
+            case 'timer':
+                var time = parseInt(args[1]);
+                line = line.replace('timer ', '');
+                line = line.replace(args[1] + ' ', '');
+                var reason = line;
+                if (line == '') {
+
+                    message.channel.send('Ti manderò un messaggio fra ' + time + ' secondi.');
+                    setTimeout(function() {
+                        message.reply("il timer è finito.");
+                    }, time * 1000);
+
+                } else {
+                    message.channel.send('Ti manderò un messaggio fra ' + time + ' secondi, per il seguente motivo: "' + reason + '".');
+                    setTimeout(function() {
+                        message.reply('il timer è finito. Motivo: "' + reason + '".');
+                    }, time * 1000);
+                }
+                break;
+            case 'lunghezza':
+                line = line.replace('lunghezza ', '');
+                var lunghezzamess = line.length;
+                message.channel.send('Il messaggio ha ' + lunghezzamess + ' caratteri.');
+                break;
+            case 'ban':
+                var x, t;
+                for (x in admin) {
+                    if (x.includes(message.author.id)) {
+                        t = 1;
+                    } else t = 0;
+                }
+                if (t == 1) {
+                    const user = message.mentions.users.first();
+                    message.guild.ban(user);
+                } else message.channel.send("Mi dispiace Testa di Cazzo non li puoi fare...");
+                break;
+            case 'addrole':
+                try {
+                    user.addRole(role);
+                    message.channel.send('Ho aggiunto ' + user + " al ruolo " + role + ".");
+                } catch (error) {
+                    console.log(error);
+                    message.channel.send("Sono troppo insicura per farlo...");
+                    message.channel.send("ah, è ho anche riscontrato un errore");
+                    //message.channel.send("@Krammm , @NinjaSbrillo non sapete neanche programmarmi")
+                }
+                break;
+            case 'removerole':
+                try {
+                    user.removeRole(role);
+                    message.channel.send('Ho tolto ' + user + " dal ruolo " + role + ".");
+                } catch (error) {
+                    console.log(error);
+                    message.channel.send("Sono troppo insicura per farlo...");
+                    message.channel.send("ah, è ho anche riscontrato un errore");
+                    //message.channel.send("@Krammm , @NinjaSbrillo non sapete neanche programmarmi")
+                }
+                break;
+            case 'clear':
+                try {
+                    async function clear() {
+                        message.delete();
+                        const fetched = await message.channel.fetchMessages({ limit: 99 });
+                        message.channel.bulkDelete(fetched);
+                    }
+                    clear();
+                } catch (error) {
+                    console.log(error);
+                    message.channel.send("Errore nell'eliminazione dei messaggi.");
+                }
+                break;
+            default:
+                message.channel.send("Command: " + command + " Line: " + line);
+                break;
+        }
+    }
 
 
 
 
 
-  if (command.includes('lovecchio') || command.includes('lovi')) {
+    if (command.includes('lovecchio') || command.includes('lovi')) {
 
-    var x = Math.random();
-    x = x * 4;
-    x = parseInt(x);
-    message.channel.send(insulti[x]);
+        var x = Math.random();
+        x = x * 4;
+        x = parseInt(x);
+        message.channel.send(insulti[x]);
 
     } else if (command.includes('trih')) {
 
-      message.channel.send('Il magnifico trih :heart_eyes:');
+        message.channel.send('Il magnifico trih :heart_eyes:');
 
     } else if (command.includes('esci il cazzo?')) {
 
-      message.channel.send('8===========>');
+        message.channel.send('8===========>');
 
     } else if (command.includes('dio')) {
 
-      message.channel.send('Non nominare il nome di TRIA invano!');
+        message.channel.send('Non nominare il nome di TRIA invano!');
 
     } else if (command.includes('bravo bot')) {
 
-      message.reply('Grazie :blush:');
+        message.reply('Grazie :blush:');
 
-    } else if (command.includes('si lavora e si produce')){
+    } else if (command.includes('si lavora e si produce')) {
 
-      message.channel.send('per tria il nostro duce');
+        message.channel.send('per tria il nostro duce');
 
-    } else if(command.includes('heil hitler')){
+    } else if (command.includes('heil hitler')) {
 
-      message.channel.send('heil :hitler: :nazi:');
+        message.channel.send('heil :hitler: :nazi:');
 
     }
 
@@ -165,4 +171,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.BOT_TOKEN); //BOT_TOKEN is the Client Secret
