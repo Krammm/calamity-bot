0c5244e2558949a52d0212513f9e810a8d438053
Marco <42967229+Krammm@users.noreply.github.com>
2020-10-12 17:27:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,73 +2,35 @@ const Discord = require('discord.js');
 // boh
 const client = new Discord.Client();
 const prefix = '!';
-const admin = [509779445557493821, 544579907133636619, 296353825550237697, 211131331155984385];
-var insulti = ['Quel frocione di lovy', ' :poop:', 'Ecco adesso mi devo fare la doccia...', 'Perchè parli della merda?'];
-var command_channel = 704623913614704731;
 
 var mutedUsers = new Array();
-//var mutedUsersIndex = 0;
 
 client.on('ready', () => {
-    console.log('I am ready!');
+    console.log('Il bot è online.');
 });
 
 client.on('message', message => {
 
-    if (message.author.id === client.user.id) { return; }
-
-    if (message.isMemberMentioned(client.user)) {
-
-        message.reply('cazzo vuoi?');
-
-    }
-
-    var command = message.content.toLowerCase()
-    
-    // Delete music bots commands in a non-music command channel
-    
- /*   for (x in mutedUsers) {
-                    if (mutedUsers[x] == message.author.id) {
-                        message.delete();
-                    }
-    }
-
-
-
-*/
-
-    mutedUsers.forEach(checkIfUserIsMuted(x) {
-
-   // mutedUsers.forEach(x => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return; // The bot won't respond to a non-command or a message sent by a bot
+   
+    mutedUsers.forEach(x => { 					// Trying a muting function; commands are muted, too 
 
         if (mutedUsers[x] == message.author.id) {
             message.delete();
+            return();
         }
     });
     
-    if ((command.startsWith("-") || command.startsWith(".")) && !message.channel.name.includes("music-commands")) {
-        message.delete();
-    }
-    if ((message.author.id == 235088799074484224) && !message.channel.name.includes("music-commands")) {
-        message.delete();
-    }
-    if ((message.author.id == 234395307759108106) && !message.channel.name.includes("music-commands")) {
-        message.delete();
-    }
-    
-    
-    if (command.startsWith(prefix)) { // Se il messaggio è un comando
-        //if(!command.channel.name.includes("💻comandi💻")){
-        //  message.reply("testa di cazzo non ti permettere!");
-        //}
+    var text = message.content.toLowerCase() // The var "text" is the main string that the bot is going to check
 
-        var line = command;
-        line = line.substring(1);
-        var args = message.content.trim().split(/ +/g);
-        command = args[0];
-        command = command.substring(1);
-        var user = message.mentions.members.first();
-        let role = message.guild.roles.find(r => r.name === args[2]);
+   	 if ((text.startsWith("-") || command.startsWith(".")) && !message.channel.name.includes("music-commands")) { // Deleting music commands outside of #music-commands
+      	 message.delete();
+      	 return();
+    }
+    
+   		const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  		const command = args.shift().toLowerCase();
+  		var line = args.join(" ");
 
         switch (command) {
             case 'aiuto':
@@ -99,49 +61,13 @@ client.on('message', message => {
 
                 } else {
                     message.channel.send('Ti manderò un messaggio fra ' + time + ' secondi, per il seguente motivo: "' + reason + '".');
-                    setTimeout(function() {
-                        message.reply('il timer è finito. Motivo: "' + reason + '".');
-                    }, time * 1000);
+                    setTimeout(function() { message.reply('il timer è finito. Motivo: "' + reason + '".'); }, time * 1000);
                 }
                 break;
             case 'lunghezza':
                 line = line.replace('lunghezza ', '');
                 var lunghezzamess = line.length;
                 message.channel.send('Il messaggio ha ' + lunghezzamess + ' caratteri.');
-                break;
-            case 'ban':
-                var x, t;
-                for (x in admin) {
-                    if (admin[x] == message.author.i) {
-                        t = 1;
-                    } else t = 0;
-                }
-                if (t == 1) {
-                    let user = message.mentions.users.first();
-                    message.guild.ban(user);
-                } else message.channel.send("Non hai abbastanza permessi per farlo.");
-                break;
-            case 'addrole':
-                try {
-                    user.addRole(role);
-                    message.channel.send('Ho aggiunto ' + user + " al ruolo " + role + ".");
-                } catch (error) {
-                    console.log(error);
-                    message.channel.send("Sono troppo insicura per farlo...");
-                    message.channel.send("ah, è ho anche riscontrato un errore");
-                    //message.channel.send("@Krammm , @NinjaSbrillo non sapete neanche programmarmi")
-                }
-                break;
-            case 'removerole':
-                try {
-                    user.removeRole(role);
-                    message.channel.send('Ho tolto ' + user + " dal ruolo " + role + ".");
-                } catch (error) {
-                    console.log(error);
-                    message.channel.send("Sono troppo insicura per farlo...");
-                    message.channel.send("ah, è ho anche riscontrato un errore");
-                    //message.channel.send("@Krammm , @NinjaSbrillo non sapete neanche programmarmi")
-                }
                 break;
             case 'clear':
                 try {
@@ -158,20 +84,15 @@ client.on('message', message => {
                 break;
             case 'mute':
                 try {
-                    user = message.mentions.users.first();
-
-                    console.log(user);
+                    var user = message.mentions.users.first();
 
                     mutedUsers.push(user.id);
                     
                     mutedUsers.forEach(element => {
                         console.log(element);
                     });
-                    //mutedUsersIndex = mutedUsersIndex + 1;
 
                     message.channel.send(user);
-                    //mutedUsers[mutedUsersIndex] = user;
-                    //mutedUsersIndex = mutedUsersIndex + 1;
 
                 } catch (error) {
                     console.log(error);
@@ -179,48 +100,11 @@ client.on('message', message => {
                 }
                 break;
             default:
-                message.channel.send("Command: " + command + " Line: " + line);
+                message.channel.send("DEBUG --- Command: " + command + " Line: " + line);
                 break;
         }
     }
 
-
-
-
-/*
-    if (command.includes('lovecchio') || command.includes('lovi')) {
-
-        var x = Math.random();
-        x = x * 4;
-        x = parseInt(x);
-        message.channel.send(insulti[x]);
-
-    } else if (command.includes('trih')) {
-
-        message.channel.send('Il magnifico trih :heart_eyes:');
-
-    } else if (command.includes('esci il cazzo?')) {
-
-        message.channel.send('8===========>');
-
-    } else if (command.includes('dio')) {
-
-        message.channel.send('Non nominare il nome di TRIA invano!');
-
-    } else if (command.includes('bravo bot')) {
-
-        message.reply('Grazie :blush:');
-
-    } else if (command.includes('si lavora e si produce')) {
-
-        message.channel.send('per tria il nostro duce');
-
-    } else if (command.includes('heil hitler')) {
-
-        message.channel.send('heil :hitler: :nazi:');
-
-    }
-*/
 });
 
 
